92e46aa8803aa5f455f681f1c24ee0090d2e8b80
johnreaganmoore <johnreaganmoore@gmail.com>
2019-01-12 06:31:18 -0500

    Run marker icon migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180204205853) do
+ActiveRecord::Schema.define(version: 20190112111414) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 20180204205853) do
     t.string "season_end"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "marker_icon"
   end
 
   create_table "teams", force: :cascade do |t|
